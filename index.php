fd315aff0b7152f677ac51ef8a1e1d7f28f90f9d
Alifa Fathia <alifa.fathia@gmail.com>
2017-05-08 19:52:14 +0700

    logout page

--- index.php
+++ index.php
@@ -3,15 +3,9 @@
     <head>
     <title>SIRIMA</title>
       <!-- Bootstrap scripts -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-      <!-- Special version of Bootstrap that is isolated to content wrapped in .bootstrap-iso -->
-      <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
-
-      <!--Font Awesome (added because you use icons in your prepend/append)-->
-      <link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
+      <link rel="stylesheet" href="./bootstrap-3.3.7/css/bootstrap.min.css">
+      <script src="./bootstrap-3.3.7/js/jquery.min.js"></script>
+      <script src="./bootstrap-3.3.7/js/bootstrap.min.js"></script>
 
       <!-- Other scripts -->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
@@ -37,26 +31,31 @@
       <div class="form">
         <div class="thumbnail"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/hat.svg"/></div>
         <form class="register-form">
-          <span>Registrasi Akun SIRIMA<br></span>
+          <span><h2>Registrasi Akun SIRIMA</h2><br></span>
           <br>
           <div class="form-group">
+            <span><p>username</p></span>
             <input type="text" class="form-control" id="username" placeholder="username">
           </div>
           <div class="form-group">
+            <span><p>password</p></span>
             <input type="password" class="form-control" id="password" placeholder="password">
           </div>
           <div class="form-group">
+            <span><p>ulangi password</p></span>
             <input type="password" class="form-control" id="repeatpassword" placeholder="ulangi password">
           </div>
           <div class="form-group">
+            <span><p>nama lengkap</p></span>
             <input type="text" class="form-control" id="fullname" placeholder="nama lengkap">
           </div>
           <div class="form-group">
+            <span><p>nomor identitas</p></span>
             <input type="text" class="form-control" id="idnumber" placeholder="no. identitas">
           </div>
           <div class="form-group">
+            <span><p>jenis kelamin</p></span>
             <select class="form-control" id="jeniskelamin">
-              <option selected>jenis kelamin</option>
               <option value="L">laki-laki</option>
               <option value="P">perempuan</option>
             </select>
@@ -64,16 +63,20 @@
 
           <!-- date picker -->
             <div class="form-group">
+              <span><p>tanggal lahir</p></span>
               <input class="form-control" id="date" name="date" placeholder="tanggal lahir: MM/DD/YYY" type="date"/>
             </div>
 
           <div class="form-group">
+            <option selected>alamat</option> 
             <textarea class="form-control" id="address" rows="2" placeholder="alamat"></textarea>
           </div>
           <div class="form-group">
+            <option selected>alamat e-mail</option>
             <input class="form-control" name="email" type="text" placeholder="alamat e-mail"/>
           </div>
           <div class="form-group">
+            <option selected>ulangi alamat e-mail</option>
             <input class="form-control" name="repeatemail" type="text" placeholder="ulangi e-mail"/>
           </div>
           <button type="submit">create account</button>
@@ -94,27 +97,9 @@
         <source src="http://andytran.me/A%20peaceful%20nature%20timelapse%20video.mp4" type="video/mp4"/>
       </video>
 
-      <!-- Include Date Range Picker -->
-      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
-
       <!-- JQuery Script -->
       <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
       <script src="js/index.js"></script>
-
-      <script>
-      //   $(document).ready(function(){
-      //     var date_input=$('input[name="date"]'); //our date input has the name "date"
-      //     var container=$('.bootstrap-iso form-group').length>0 ? $('.bootstrap-iso form').parent() : "body";
-      //     var options={
-      //       format: 'mm/dd/yyyy',
-      //       container: container,
-      //       todayHighlight: true,
-      //       autoclose: true,
-      //     };
-      //     date_input.datepicker(options);
-      //   })
-      // </script>
     </body>
   </html>
